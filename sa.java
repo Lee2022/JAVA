f6f4511c1fd3a4cb0a4ab55dc406d9cf4048f6a1
Lee2022 <54237402+Lee2022@users.noreply.github.com>
2020-06-08 09:21:56 +0900

    java

---
+++ sa.java
@@ -0,0 +1,20 @@
+package a;
+
+public class sa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a=1;
+		int	sum = 0;
+		
+		for (a=1;a<=10;a++){
+		
+			System.out.print(a+" ");
+			sum +=a; 
+		}
+		System.out.println(sum);
+	}
+
+}
